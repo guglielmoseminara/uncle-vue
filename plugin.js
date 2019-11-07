fe24f1b3596fb3fb4c61549bdc481497f5071f46
guglielmoseminara <guglielmo.seminara@gmail.com>
2019-11-07 12:56:10 +0100

    fix(plugin): created plugin

---
+++ plugin.js
@@ -0,0 +1,54 @@
+import { Loader } from './index';
+
+export default {
+    install(Vue, options) {
+        var loader = new Loader();
+        loader.init();
+        const app = loader.getParser().getApp(options.app);
+        Vue.prototype.$uncle = function() {
+            return {
+                getNav: function(navName) {
+                    return app.getNav(navName);
+                },
+                getView: function(viewName) {
+                    return app.getView(viewName);
+                },
+                getList: function(listName) {
+                    return app.getList(listName);
+                },
+                getFilter: function(filterName) {
+                    return app.getFilter(filterName);
+                },
+                getForm: function(formName) {
+                    return app.getForm(formName);
+                },
+                getSdk: function() {
+                    return app.getSdk();
+                },
+                getSummary: function(summaryName) {
+                    return app.getSummary(summaryName);
+                },
+                initRoutes: function() {
+                    var routes = app.getRoutes();
+                    routes = routes.map((route) => {
+                        route.url = route.url.split('/').map((routeFragment) => {
+                            let routeFragmentMatch = routeFragment.match(/\{.+?\}/g);
+                            if (routeFragmentMatch && routeFragmentMatch.length > 0) {
+                                return ':'+routeFragmentMatch[0].slice(1,-1);
+                            }
+                            return routeFragment;
+                        }).join('/');
+                        return {
+                            path: route.url,
+                            component: require('./views/'+route.view.name+'.vue').default,
+                            name: route.name,
+                            props: {view: route.view.name}
+                        }
+                    });
+                    options.router.addRoutes(routes);
+                }
+            }
+        }();
+        Vue.prototype.$uncle.initRoutes();
+    }
+}
