bc08a18bdb6a37291be13e538c2179b70226f1f1
guglielmoseminara <guglielmo.seminara@gmail.com>
2020-03-09 16:28:03 +0100

    fix translate nav item

--- src/nav_item.js
+++ src/nav_item.js
@@ -8,10 +8,11 @@ export default class NavItem extends BaseElement {
 
     build(navEl, navItemName) {
         this.navItemEl = navEl.querySelector(`nav-item[name="${navItemName}"]`);
+        const languageProvider = this.serviceManager.getLanguageProvider();
         this.appEl = this.navItemEl.closest('app');
         this.route = this.navItemEl.getAttribute('route') ? this.builder.getRoute(this.appEl, this.navItemEl.getAttribute('route')) : null;
         this.action = this.navItemEl.getAttribute('action') ? this.builder.getAction(this.navItemEl.getAttribute('action')) : null;
-        this.text = this.navItemEl.getAttribute('text') || this.navItemEl.innerHTML;
+        this.text = this.navItemEl.getAttribute('text') ? languageProvider.parse(this.navItemEl.getAttribute('text')) : languageProvider.parse(this.navItemEl.innerHTML);
         this.icon = this.navItemEl.getAttribute('icon');
         this.name = navItemName;
         return this;
